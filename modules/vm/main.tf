2f9863bbd69ed04b3005520d9b953ec4915cfe10
Evandro Pomatti <evandro.pomatti@gmail.com>
2024-03-16 14:01:15 -0300

    client vm public ip

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -1,3 +1,10 @@
+resource "azurerm_public_ip" "windows11" {
+  name                = "pip-windows11"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  allocation_method   = "Static"
+}
+
 resource "azurerm_network_interface" "windows11" {
   name                = "nic-windows11"
   resource_group_name = var.resource_group_name
@@ -7,6 +14,7 @@ resource "azurerm_network_interface" "windows11" {
     name                          = "windows11"
     subnet_id                     = var.subnet
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.windows11.id
   }
 
   lifecycle {
