ae75ba63457d14b33d900f3c95247774ee384c8b
Evandro Pomatti <evandro.pomatti@gmail.com>
2024-07-08 21:20:57 -0300

    feat: private endpoint connections

--- modules/cr/main.tf
+++ modules/cr/main.tf
@@ -31,7 +31,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "registry" {
   resource_group_name   = var.resource_group_name
   private_dns_zone_name = azurerm_private_dns_zone.registry.name
   virtual_network_id    = var.vnet_id
-  registration_enabled  = true
+  registration_enabled  = false
 }
 
 # Account Endpoint
